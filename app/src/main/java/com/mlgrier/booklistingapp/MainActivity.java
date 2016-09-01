49cdfd4fcc57da2d49855e90f8a673deae8ffd42
Marco Grier <mlgrier@gmail.com>
2016-09-01 14:25:27 -0500

    String storing variable added

--- app/src/main/java/com/mlgrier/booklistingapp/MainActivity.java
+++ app/src/main/java/com/mlgrier/booklistingapp/MainActivity.java
@@ -3,10 +3,11 @@ package com.mlgrier.booklistingapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
@@ -14,11 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    private EditText searchKeyword;
-
-    public void onClick (View view) {
+        final Button button = (Button) findViewById(R.id.getListBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userInput = (EditText) findViewById(R.id.bookKeyword);
+                String userBookSearch = userInput.getText().toString();
+                Toast.makeText(getApplicationContext(), (userBookSearch), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
