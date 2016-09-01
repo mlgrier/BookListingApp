7a2d5cda9133b69b95ac8c1e885baf0e31d4dfba
Marco Grier <mlgrier@gmail.com>
2016-09-01 16:59:15 -0500

    Added new java class "Book"

--- app/src/main/java/com/mlgrier/booklistingapp/ListActivity.java
+++ app/src/main/java/com/mlgrier/booklistingapp/ListActivity.java
@@ -1,9 +1,25 @@
 package com.mlgrier.booklistingapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -17,5 +33,158 @@ public class ListActivity extends AppCompatActivity {
         String userBookSearchInfo = intent.getStringExtra("userBook2");
 
         Toast.makeText(getApplicationContext(), userBookSearchInfo, Toast.LENGTH_LONG).show();
+
     }
+
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
+    private class BookListAsyncTask extends AsyncTask<URL, Void, Book> {
+
+        @Override
+        protected Book doInBackground(URL... urls) {
+            // Create URL object
+            URL url = createUrl(USGS_REQUEST_URL);
+
+            // Perform HTTP request to the URL and receive a JSON response back
+            String jsonResponse = "";
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                // TODO Handle the IOException
+            }
+
+            // Extract relevant fields from the JSON response and create an {@link Event} object
+            Book earthquake = extractFeatureFromJson(jsonResponse);
+
+            // Return the {@link Event} object as the result fo the {@link BookListAsyncTask}
+            return earthquake;
+        }
+
+        /**
+         * Update the screen with the given earthquake (which was the result of the
+         * {@link BookListAsyncTask}).
+         */
+        @Override
+        protected void onPostExecute(Book earthquake) {
+            if (earthquake == null) {
+                return;
+            }
+
+            updateUi(earthquake);
+        }
+
+        /**
+         * Returns new URL object from the given string URL.
+         */
+        private URL createUrl(String stringUrl) {
+            URL url = null;
+            try {
+                url = new URL(stringUrl);
+            } catch (MalformedURLException exception) {
+                Log.e(LOG_TAG, "Error with creating URL", exception);
+                return null;
+            }
+            return url;
+        }
+
+        /**
+         * Make an HTTP request to the given URL and return a String as the response.
+         */
+        private String makeHttpRequest(URL url) throws IOException {
+            String jsonResponse = "";
+
+            //if the url is null, then return early.
+            if (url == null) {
+                return jsonResponse;
+            }
+
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+
+                //if the request was successful (response code 200),
+                // then read the input stream and parse the response.
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+                }
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (inputStream != null) {
+                    // function must handle java.io.IOException here
+                    inputStream.close();
+                }
+            }
+            return jsonResponse;
+        }
+
+        /**
+         * Convert the {@link InputStream} into a String which contains the
+         * whole JSON response from the server.
+         */
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
+
+        /**
+         * Return an {@link Book} object by parsing out information
+         * about the first earthquake from the input earthquakeJSON string.
+         */
+        private Book extractFeatureFromJson(String earthquakeJSON) {
+            //if the JSON string is empty or null, then return early.
+            if (TextUtils.isEmpty(earthquakeJSON)){
+                return null;
+            }
+
+            try {
+                JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+                JSONArray featureArray = baseJsonResponse.getJSONArray("features");
+
+                // If there are results in the features array
+                if (featureArray.length() > 0) {
+                    // Extract out the first feature (which is an earthquake)
+                    JSONObject firstFeature = featureArray.getJSONObject(0);
+                    JSONObject properties = firstFeature.getJSONObject("properties");
+
+                    // Extract out the title, time, and tsunami values
+                    String title = properties.getString("title");
+                    long time = properties.getLong("time");
+                    int tsunamiAlert = properties.getInt("tsunami");
+
+                    // Create a new {@link Event} object
+                    return new Book(title, time, tsunamiAlert);
+                }
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "Problem parsing the earthquake JSON results", e);
+            }
+            return null;
+        }
+    }
+
+
 }
