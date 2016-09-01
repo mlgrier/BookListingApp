796364d3d00abe175e3e742ee5f0ec809b8928e1
Marco Grier <mlgrier@gmail.com>
2016-09-01 12:38:42 -0500

    Updating app

--- app/src/main/java/com/mlgrier/booklistingapp/MainActivity.java
+++ app/src/main/java/com/mlgrier/booklistingapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mlgrier.booklistingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    private EditText searchKeyword;
+
+    public void onClick (View view) {
+
+    }
+
 }
